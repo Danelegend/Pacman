a476b5ecad25a79d5550de4f4a191af91da7fb08
Danelegend <43459662+Danelegend@users.noreply.github.com>
2020-06-18 12:08:12 +1000

    Update ButtonClickEvent.java

--- src/me/dane/pacman/events/ButtonClickEvent.java
+++ src/me/dane/pacman/events/ButtonClickEvent.java
@@ -1,6 +1,7 @@
 package me.dane.pacman.events;
 
 import me.dane.pacman.statics.ScreenHandler;
+import me.dane.pacman.statics.StaticWindow;
 import me.dane.pacman.util.Window;
 
 import javax.swing.*;
@@ -9,15 +10,24 @@ import java.awt.event.MouseListener;
 
 public class ButtonClickEvent implements MouseListener {
 
-    private JFrame f;
-    private Window w;
+    private JFrame menu;
 
-    public ButtonClickEvent(JFrame f, Window w) {
-        this.f = f;
-        this.w = w;
+    public ButtonClickEvent(JFrame menu) {
+        System.out.println("yo");
+        this.menu = menu;
     }
 
     public void mouseClicked(MouseEvent e) {
+        if (ScreenHandler.getScreenNum() == 1) {
+
+            if (e.getX() >= 160 + 450 && e.getX() <= 160+700+450 && e.getY() >= 500 && e.getY() <= 500 + 100) {
+                ScreenHandler.setScreenNum(2);
+                System.out.println("test");
+                menu.dispose();
+                StaticWindow.getWindow().checker();
+            }
+        }
+
         if (ScreenHandler.getScreenNum() == 2) {
 
         }
